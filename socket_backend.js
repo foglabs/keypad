dd54b94a57f421eb4164b5c638f8f96815eb2448
foglabs <foglaboratories@gmail.com>
2019-12-01 19:06:48 -0500

    'connected...'

--- socket_backend.js
+++ socket_backend.js
@@ -2,9 +2,20 @@ const webSocketsServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
 
+// I'm maintaining all active connections in this object
+const clients = {};
+
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
 // Spinning the http server and the websocket server.
 const server = http.createServer();
 server.listen(webSocketsServerPort);
+
+console.log('kickin that ass bro')
 const wsServer = new webSocketServer({
   httpServer: server
 });
@@ -16,4 +27,14 @@ wsServer.on('request', function(request) {
   const connection = request.accept(null, request.origin);
   clients[userID] = connection;
   console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
+
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+        console.log('Received Message: ' + message.utf8Data);
+        connection.sendUTF(message.utf8Data);
+    } else if (message.type === 'binary') {
+        console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+        connection.sendBytes(message.binaryData);
+    }
+  });
 });
--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-const BACKEND = 'ws://' + window.location.hostname + ':8000';
-
 // may not need this
 // import axios_lib from 'axios';
 // const axios = axios_lib.create({
 //   baseURL: BACKEND + ':4000',
 // });
-
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 // 'ws://localhost:8000'
+const BACKEND = 'ws://' + window.location.hostname + ':8000';
 const client = new W3CWebSocket(BACKEND);
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.onClick=this.onClick.bind(this);
+    console.log('cool')
+  }
   componentWillMount() {
     client.onopen = () => {
       console.log('WebSocket Client Connected');
@@ -21,14 +24,31 @@ class App extends Component {
       console.log(message);
     };
   }
+
+  onClick(){
+    console.log('I fucking clicked stupid')
+    if(client.readyState === client.OPEN){
+      client.send('but for your ass stupid');
+    }
+  }
   
   render() {
+    let keys =
+
     return (
-      <div>
-        Practical Intro To WebSockets.
-      </div>
+      <button onClick={ () => this.onClick() }>
+        suck it
+      </button>
     );
   }
+}
+
+function Key (props){
+  return (
+    <div onClick={ props.onClick } >
+      { props.number }
+    </div>
+  );
 }
 
 export default App;
